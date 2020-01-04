c5ba091accc5ae1e22abb219c83b09fb59d7bcc6
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-04 07:18:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const animal = "dog"
 
+// const is also block-scoped restricts over-writing variables. It does not mean the value it holds is immutable, just that the variable identifier cannot be reassigned. const variables also cannot be declared a without assigning its (constant) value
+
 function myAnimal() {
   return animal
 }
 
 function yourAnimal() {
-  // let animal = 'cat'
-  const animal = 'cat'
+  let animal = 'cat'
+  // const animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+
+// let variables are block-scoped, and allows you to declare variables that are limited in scope to the block, statement, or expression on which it is used
 
 function add2(n) {
   const two = 2
